1530b7cac836a64bd0914af8648a96ba25fcc191
Hira-kiran <hirakiran224@gmail.comgit config --global user.email hirakiran224@gmail.com>
2023-11-12 20:44:56 +0500

    class 1

---
+++ main.dart
@@ -0,0 +1,26 @@
+void main() {
+  // Make first program “hello world”?
+
+  print("Hello world");
+  print("FLUTTER");
+  print("DART");
+
+  print('hira kiran');
+  print('hira kiran');
+  print('hira kiran');
+  print('hira kiran');
+  print('hira kiran');
+}
+
+
+
+/*
+Lecture # 1:
+•	What is dart?
+•	How to install Vs code?
+•	What is Dartpad?
+•	Install Extensions
+•	Make first program “hello world”?
+•	How to write comments?
+Assignment
+1)	Write a program to print all your family members’ name.*/
