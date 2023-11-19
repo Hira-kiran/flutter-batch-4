62ab15bf7ff120f7beb06e2a820b84de482ff99a
Hira-kiran <hirakiran224@gmail.comgit config --global user.email hirakiran224@gmail.com>
2023-11-19 20:35:33 +0500

    class 3

---
+++ class3.dart
@@ -0,0 +1,68 @@
+void main() {
+  // •	Datatypes (bool, List, Map)
+
+  bool a = false;
+  print(a);
+
+  List<String> list = ["flutter", "dart", "ui", "fjf", "fjfk"];
+  print(list.length);
+  print(list);
+  print(list[0]);
+
+  Map map = {
+    "name": "hira",
+    "email": "hira@gmail.com",
+    "password": 1233,
+    "reg #": 38743857,
+  };
+  print(map["name"]);
+// •	Differentiate between variable and dynamic
+
+  var x = "djh";
+  x = "kjfk";
+  x = "nv";
+  print(x);
+
+  dynamic y = "hrhru";
+  y = 37;
+  y = 467.47;
+  y = false;
+  print(y);
+
+  // •	Relational operators
+
+  // >, <, >=, <=, ==, !=
+
+  int b = 20;
+  int c = 30;
+  print(b == c);
+
+  // •	Concatenation
+
+  String name = "hdjf  ";
+  String dhj = "jdhf " + "sjfhf  " + "djjhf  ";
+  print(name + dhj);
+
+  print("***");
+  print("**********");
+
+  // pi r^2
+  int r = 30;
+  double areaOfSq = 3.14 * r * r;
+  print(areaOfSq);
+}
+
+
+
+// Lecture # 3:
+// •	Datatypes (bool, List, Map)
+// •	Differentiate between variable and dynamic
+// •	Relational operators
+// •	Concatenation
+
+// Assignment	
+// 1)	Write a program in which make rectangle using print statement (*)?
+// 2)	Write a program to perform all relational operators (>, <,>=, <=).
+// 3)	Write a program to subtract two constant numbers.
+// 4)	Write a program to Calculate Area of Circle.
+// 5)	Write a program to Calculate Area of square.
