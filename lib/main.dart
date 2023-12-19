8d03d640d5b8dc3db1cd2a23cc9bc1e9e41eb04b
Hira-kiran <hirakiran224@gmail.comgit config --global user.email hirakiran224@gmail.com>
2023-12-19 21:10:29 +0500

    class 1 & 2

--- lib/main.dart
+++ lib/main.dart
@@ -52,3 +52,22 @@ class MYApp extends StatelessWidget {
     );
   }
 }
+
+
+// LEC 1:
+// •	Install flutter
+// •	Creating first flutter project
+// •	Understand vs code all things
+// •	Install extensions
+// •	Summing up what we have learned so far
+// LEC 2:
+// •	Stateless widget
+// •	Hot reload and hot restart
+// •	Understand widgets
+// •	App bar widget
+// •	Container widget
+// •	Single child widgets
+// •	Multi child widgets
+// •	Row & column widgets
+// Assignment
+// •	Design UI using six containers
